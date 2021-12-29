6e75c78ff49652bab65a45029d75590cf20ad932
Moh Amilin <amilinmoh99@gmail.com>
2021-12-29 15:45:36 +0700

    update : perhitungan dart

--- learn_dart/bin/learn_dart.dart
+++ learn_dart/bin/learn_dart.dart
@@ -1,5 +1,15 @@
 import 'package:learn_dart/learn_dart.dart' as learn_dart;
 
 void main(List<String> arguments) {
+  // print('object');
   print('Hello world: ${learn_dart.calculate()}!');
+
+  // perhitungan di dart
+  print(2 + 3);
+  print(2 * 3);
+  print(2 - 6);
+  print(7 - 2);
+  print(4 / 2);
+  print(5 / 3);
+  print(-3 / 5);
 }
